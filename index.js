1407931a00774af4a791d73db74af2f07a944005
LeoChen517 <work880517@gmail.com>
2025-05-20 22:00:56 +0800

    feat:basic docs settings
    後端專案基礎文件建立

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    method: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+app.use(express.json());
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`server running on port ${PORT}`);
+});
---
+++ src/index.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+
+const db = require("../../db/index");
+const { eq } = require("drizzle-orm");
+
+module.exports = router;

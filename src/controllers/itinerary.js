6baa2adec44348e44cd23e032b308b4d853736bf
mmaysu <wannadonna34@gmail.com>
2025-06-02 16:20:02 +0800

    chore: seperate routes and ctrl

---
+++ src/controllers/itinerary.js
@@ -0,0 +1,71 @@
+const { db } = require('../db/index')
+const { itineraryPlaces } = require('../db/schema')
+const { and, eq } = require('drizzle-orm')
+
+async function addPlace(req, res){
+  const { itineraryId, name, address } = req.body
+  if (!itineraryId || typeof name !== 'string' || !name.trim()) {
+    return res
+      .status(400)
+      .json({ success: false, message: '缺少必要參數或參數錯誤' })
+  }
+
+  try {
+    await db.insert(itineraryPlaces).values({
+      itineraryId,
+      name,
+      address
+    })
+    res.json({ success: true })
+  } catch (err) {
+    console.error('資料庫寫入錯誤:', err)
+    res.status(500).json({ success: false, message: '伺服器錯誤' })
+  }
+}
+
+async function deletePlace(req, res){
+  console.log('▶️ 收到 DELETE /api/itinerary/place', req.query)
+  const { itineraryId, name } = req.query
+
+  if (!itineraryId || !name) {
+    return res.status(400).json({ success: false, message: '缺少必要參數' })
+  }
+
+  try {
+    await db
+      .delete(itineraryPlaces)
+      .where(
+        and(
+          eq(itineraryPlaces.itineraryId, Number(itineraryId)),
+          eq(itineraryPlaces.name, name)
+        )
+      )
+
+    res.json({ success: true })
+  } catch (error) {
+    console.error('刪除景點失敗：', error)
+    res.status(500).json({ success: false, message: '刪除失敗' })
+  }
+}
+
+async function getPlaces(req, res){
+  const { itineraryId } = req.query;
+
+  if (!itineraryId) {
+    return res.status(400).json({ success: false, message: '缺少 itineraryId' });
+  }
+
+  try {
+    const places = await db
+      .select()
+      .from(itineraryPlaces)
+      .where(eq(itineraryPlaces.itineraryId, Number(itineraryId)));
+
+    res.json({ success: true, places });
+  } catch (err) {
+    console.error('查詢景點失敗:', err);
+    res.status(500).json({ success: false, message: '伺服器錯誤' });
+  }
+};
+
+module.exports = {addPlace, deletePlace, getPlaces}

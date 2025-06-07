a8d6c3aee4aa88e66c30592aea1dda5c5be3987b
Ninahsuuuuu <ninaaahsu@gmail.com>
2025-06-07 20:46:06 +0800

    feat:加入token

--- src/routes/scheduleRoutes.js
+++ src/routes/scheduleRoutes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 
 const { createSchedule } = require("../controllers/scheduleController");
 const uploadCover = require("../middlewares/uploadCover");
-const authenticateToken = require("../middlewares/authMiddleware")
+const { authenticateToken } = require("../middlewares/authMiddleware")
 
 router.post("/", authenticateToken, uploadCover.single("cover"), createSchedule);
 

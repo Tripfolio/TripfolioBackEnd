ba6efe97e9499c7fedf6d250535802de16a196ee
pearl <pearl81931@gmail.com>
2025-06-16 11:17:00 +0800

    feat:itinerary edit function

--- index.js
+++ index.js
@@ -1,21 +1,32 @@
+require('dotenv').config();
+
 const express = require('express');
 const cors = require('cors');
+
+const { db } = require('./src/config/db');
+const { travelSchedules } = require('./src/models/scheduleSchema');
+const { eq } = require('drizzle-orm');
+
 const memberRoutes = require('./src/routes/memberRoutes');
-const itineraryRouter = require('./src/routes/itinerary')
+const itineraryRouter = require('./src/routes/itinerary');
 const emailPreferencesRoute = require('./src/routes/emailPreferencesRoute');
+const updateScheduleRoutes = require('./src/routes/updateScheduleRoutes');
+const scheduleRoutes = require('./src/routes/scheduleRoutes');
+
 const app = express();
-require('dotenv').config();
 
 app.use(cors());
 app.use(express.json());
 app.use('/uploads', express.static('uploads'));
+
 app.use('/api', memberRoutes);
 app.use('/api/itinerary', itineraryRouter);
 app.use("/api/email-preferences", emailPreferencesRoute);
+app.use('/api/updateScheduleRoutes', updateScheduleRoutes);
+app.use('/api/travelSchedule', scheduleRoutes);
 
-
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`)
-})
+  console.log(`Server is now running and listening on port ${PORT}`);
+});

df6ceadc637ec0b435cfc76b7da7adce8597452f
Ninahsuuuuu <ninaaahsu@gmail.com>
2025-06-17 23:58:10 +0800

    刪除假資料

--- src/utils/generate-token.js
+++
@@ -1,7 +0,0 @@
-const jwt = require("jsonwebtoken");
-
-const payload = { id: 1, email: "test@example.com" };
-const secret = '9f0c1d6e4b2a1e0d3f7c8a9b5d6f1234567890abcdef1234567890abcdef1234';
-const token = jwt.sign(payload, secret);
-
-console.log("✅ 新 token：", token);

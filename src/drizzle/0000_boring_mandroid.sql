9b283a608c19fcf6408e635ea420b95c7da678cd
kiwiisfish <kiwiisfish@gmail.com>
2025-06-18 10:40:12 +0800

    fix:reset table

--- src/drizzle/0000_boring_mandroid.sql
+++ src/drizzle/0000_boring_mandroid.sql
@@ -1,3 +1,10 @@
+CREATE TABLE "comments" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"member_id" integer,
+	"content" text NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL
+);
+--> statement-breakpoint
 CREATE TABLE "email_preferences" (
 	"user_id" uuid PRIMARY KEY NOT NULL,
 	"on_register" boolean DEFAULT true NOT NULL,
@@ -16,7 +23,28 @@ CREATE TABLE "itinerary_places" (
 	"itinerary_id" integer,
 	"name" text,
 	"address" text,
-	"photo" text
+	"photo" text,
+	"arrival_hour" integer,
+	"arrival_minute" integer,
+	"place_order" integer
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"email" varchar(100) NOT NULL,
+	"password" varchar(255) NOT NULL,
+	"phone" varchar(20) NOT NULL,
+	"created_at" timestamp (2) DEFAULT now(),
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+CREATE TABLE "community_posts" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"member_id" integer NOT NULL,
+	"schedule_id" integer NOT NULL,
+	"cover_url" text,
+	"content" text,
+	"created_at" timestamp DEFAULT now()
 );
 --> statement-breakpoint
 CREATE TABLE "schedules" (
@@ -41,11 +69,4 @@ CREATE TABLE "members" (
 	"avatar" text
 );
 --> statement-breakpoint
-CREATE TABLE "users" (
-	"id" serial PRIMARY KEY NOT NULL,
-	"email" varchar(100) NOT NULL,
-	"password" varchar(255) NOT NULL,
-	"phone" varchar(20) NOT NULL,
-	"created_at" timestamp (2) DEFAULT now(),
-	CONSTRAINT "users_email_unique" UNIQUE("email")
-);
+ALTER TABLE "comments" ADD CONSTRAINT "comments_member_id_members_id_fk" FOREIGN KEY ("member_id") REFERENCES "public"."members"("id") ON DELETE no action ON UPDATE no action;

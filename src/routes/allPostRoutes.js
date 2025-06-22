600754517950fc51ec10fb4df2b11b8068a0d645
kiwiisfish <kiwiisfish@gmail.com>
2025-06-22 20:24:01 +0800

    fix:delete allPost

--- src/routes/allPostRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const { getAllPosts } = require("../controllers/allPostCtrl");
-
-router.get("/", getAllPosts);
-
-module.exports = router;

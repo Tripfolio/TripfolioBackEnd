4bb4eb881540d3cd48ebcc33aead33273ec4b318
May Su <wannadonna34@gmail.com>
2025-07-02 09:43:33 +0800

    Update oAuthRoute.js

--- src/routes/oAuthRoute.js
+++ src/routes/oAuthRoute.js
@@ -2,11 +2,6 @@ const express = require('express');
 const router = express.Router();
 const { googleAuth, googleAuthCallback } = require('../controllers/oAuthCtrl');
 
-router.get('/google', (req, res, next) => {
-  console.log('[OAuth] Hit /auth/google');
-  next();
-}, googleAuth);
-
 router.get('/google', googleAuth);
 
 router.get('/google/callback', googleAuthCallback);
